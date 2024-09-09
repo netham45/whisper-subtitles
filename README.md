c089a121fc872aac62bae696a9fd84c66af8819e
Nathan Ladwig <netham45@gmail.com>
2024-09-08 18:48:15 -0600

    no more ffmpeg splitting and weird file handling

--- README.md
+++ README.md
@@ -10,28 +10,33 @@ This script uses Whisper to transcribe an audio stream in real-time, providing s
 
 ## Usage
 
-```
-python subtitles.py [-h] [-m MODEL] [-d DEVICE] [-l CHUNK_LENGTH] [-n NUM_CHUNKS] source
+```bash
+python subtitles.py [-h] 
+  [-m MODEL] 
+  [-d DEVICE] 
+  [-l CHUNK_LENGTH] 
+  [-n NUM_CHUNKS]
+  source
 ```
 
 ### Arguments:
 
 - `source`: File or URL for ffmpeg to play (required)
-- `-m, --model`: Whisper model to use (default: "base.en")
-- `-d, --device`: 'cuda' for GPU or 'cpu' for CPU (default: "cuda")
+- `-m, --model`: Whisper model to use. Available options: `tiny`, `base`, `small`, `medium`, `large`, `large_v1`, `large_v2`, `large_v3`, `tiny.en`, `base.en`, `small.en`, `medium.en` (default: "base")
+- `-d, --device`: Compute device type. Available options: `cuda`, `cpu` (default: "cuda")
 - `-l, --chunk_length`: Length of chunks in seconds (default: 3)
-- `-n, --num_chunks`: Number of chunks to process (default: 2)
+- `-n, --num_chunks`: Number of chunks to process at once (default: 2)
 
 ### Example:
 
-```
-python subtitles.py https://example.com/audio_stream.mp3 -m medium -d cpu -l 5 -n 3
+```bash
+python subtitles.py https://example.com/audio_stream.mp3 -m medium.en -d cpu -l 5 -n 3
 ```
 
-This example uses the 'medium' model on CPU, processing 5-second chunks and processes the last 3 chunks.
+This example uses the 'medium.en' model on CPU, processing 5-second chunks and processes the last 3 chunks.
 
 For more information on available options, use:
 
-```
+```bash
 python subtitles.py -h
 ```
--- subtitles.py
+++ subtitles.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 """This class transcribes a live ffmpeg stream as subtitles"""
 import argparse
-import os
-import re
 import subprocess
 import tempfile
 import threading
 from enum import Enum
-from typing import Annotated, List, Optional, Union
+from typing import Annotated, List, Union
 
 import numpy as np
 import whisper
 from pydantic import AnyUrl, BaseModel, Field, FilePath, ValidationError
 
 CLEAR: str = "\033[2J\033[H"  # ANSI clear code
+WHISPER_SAMPLE_RATE: int = 16000
+FFMPEG_DATA_TYPE: type = np.int16
+FFMPEG_DATA_STRING: str = "s16le"
+FFMPEG_CHANNELS: int = 1
+FFMPEG_LOG_LEVEL: str = "fatal"
 
 class WhisperDevice(str, Enum):
     """Available Whisper Devices"""
@@ -35,25 +38,26 @@ class WhisperModel(str, Enum):
     SMALL_EN = 'small.en'
     MEDIUM_EN = 'medium.en'
 
+DEFAULT_MODEL: WhisperModel = WhisperModel.BASE_EN
+DEFAULT_DEVICE: WhisperDevice = WhisperDevice.CUDA
+DEFAULT_NUM_CHUNKS: int = 2
+DEFAULT_CHUNK_LENGTH: int = 3
+
 WhisperDeviceAnnotation = Annotated[WhisperDevice, "Compute device type."]
-
 WhisperModelAnnotation = Annotated[WhisperModel, "Whisper model to run."]
-
 ChunkLengthAnnotation = Annotated[int,
             "Chunk length in seconds for audio to be segmented into.",
             Field(strict=True, ge=0, le=10)]
-
 NumChunksAnnotation = Annotated[int,
             "Number of chunk segments to be transcribed at once.",
             Field(strict=True, ge=0, le=10)]
-
 URLFileAnnotation = Annotated[Union[AnyUrl, FilePath] , "URL or File to be streamed."]
 
 class SubtitleStreamProperties(BaseModel):
     """Subtitle Stream Properties"""
     device_type: WhisperDeviceAnnotation
     whisper_model: WhisperModelAnnotation
-    chunk_length: ChunkLengthAnnotation
+    chunk_duration: ChunkLengthAnnotation
     num_chunks: NumChunksAnnotation
     source: URLFileAnnotation
 
@@ -61,60 +65,41 @@ class Subtitles(threading.Thread):
     """Reads an ffmpeg stream and does subtitles for it"""
     __model: whisper.Whisper
     __running: bool = True
-    __file_list: List[str] = []
+    __chunks: List[np.ndarray] = []
     __stream_properties: SubtitleStreamProperties
     __temp_dir: str
+    __chunk_bytes: int
+    __process = None
 
     def __init__(self,
                  stream_properties: SubtitleStreamProperties) -> None:
         super().__init__()
         self.__stream_properties = stream_properties
         self.__temp_dir = tempfile.mkdtemp(prefix="subtitles_")
-        self.__chunk_length = int(self.__stream_properties.chunk_length)
         self.__stream_properties.num_chunks = int(self.__stream_properties.num_chunks)
+        self.__chunk_bytes = (self.__stream_properties.chunk_duration *
+                              WHISPER_SAMPLE_RATE *
+                              np.dtype(FFMPEG_DATA_TYPE).itemsize)
         print(f"Model '{self.__stream_properties.whisper_model}'")
         print(f"Device: '{self.__stream_properties.device_type}'")
         print(f"Temp Dir Path: '{self.__temp_dir}'")
-        print(f"Chunk Length: {self.__chunk_length}")
+        print(f"Chunk Duration: {self.__stream_properties.chunk_duration} seconds")
         print(f"Number of Chunks: {self.__stream_properties.num_chunks}")
+        print(f"Source {self.__stream_properties.source}")
+        print(f"Chunk Bytes: {self.__chunk_bytes}")
         print("Loading Model")
         self.__model = whisper.load_model(self.__stream_properties.whisper_model,
                                           self.__stream_properties.device_type)
         print("Loaded Model")
 
-    def __load_combined_audio(self) -> np.ndarray:
-        """Loads a list of files into a single numpy array."""
-        cmd: List[str] = ["ffmpeg",
-                          "-nostdin",
-                          "-threads", "0",
-                          "-i", "concat:" + "|".join(self.__file_list),
-                          "-f", "s16le",  # 16-bit
-                          "-ac", "1",     # mono
-                          "-acodec", "pcm_s16le", # pcm
-                          "-ar", "16000", # 16khz sample
-                          "pipe:"]
-        try:
-            out = subprocess.run(cmd, capture_output=True, check=True).stdout
-        except subprocess.CalledProcessError as e:
-            raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
-
-        return np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0
-
-    def __process_file(self, file_name) -> None:
-        """Adds a mp3 segment to the queue for processing"""
-        if file_name in self.__file_list:
-            return
-        self.__file_list.append(file_name)
-
-        if len(self.__file_list) > self.__stream_properties.num_chunks:
-            try:
-                os.unlink(self.__file_list[0])
-            except OSError:
-                print(f"\nFailed to remove temp file {self.__file_list[0]}")
-            self.__file_list = self.__file_list[-self.__stream_properties.num_chunks:]
-        if len(self.__file_list) >= self.__stream_properties.num_chunks:
-            audio = self.__load_combined_audio()
-            result = whisper.transcribe(self.__model, audio)
+    def __process_chunk(self, data: np.ndarray) -> None:
+        """Processes a chunk of audio"""
+        self.__chunks.append(data)
+        if len(self.__chunks) >= self.__stream_properties.num_chunks:
+            combined_audio: np.ndarray = (np.concatenate(self.__chunks).astype(np.float32) /
+                                          np.iinfo(FFMPEG_DATA_TYPE).max)
+            result = whisper.transcribe(self.__model, combined_audio)
+            del self.__chunks[0]
             print(f"{CLEAR}{result['text']}", end="", flush=True)
         else:
             print(f"{CLEAR}Receiving Initial Audio", end="", flush=True)
@@ -122,46 +107,34 @@ class Subtitles(threading.Thread):
     def run(self) -> None:
         """Starts ffmpeg and listens for new files from it"""
         cmd: List[str] = ["ffmpeg",
+                          "-hide_banner",
+                          "-loglevel", FFMPEG_LOG_LEVEL,
                           "-i", str(self.__stream_properties.source),
-                          "-f", "segment",
-                          "-segment_time", str(self.__stream_properties.chunk_length),
-                          "-strftime", "1",
-                         f"{self.__temp_dir}/%H-%M-%S.mp3",
-                          "-v", "verbose"]
+                          "-f", FFMPEG_DATA_STRING,
+                          "-ar", str(WHISPER_SAMPLE_RATE),
+                          "-ac", str(FFMPEG_CHANNELS),
+                          "pipe:"]
         with subprocess.Popen(
                 cmd,
                 stdout=subprocess.PIPE,
-                stderr=subprocess.STDOUT,
-                text=True,
-                bufsize=1,
-                universal_newlines=True
-            ) as process:
-            prev_file: str = ""
-            if process.stdout is None:
+                bufsize=self.__chunk_bytes,
+            ) as self.__process:
+            if self.__process.stdout is None:
                 raise RuntimeError("stdout is none")
             while self.__running:
-                line: str = process.stdout.readline()
-                if not line:
+                data: bytes = self.__process.stdout.read(self.__chunk_bytes)
+                if len(data) == 0:
+                    self.__running = False
                     break
-                match: Optional[re.Match[str]] = re.search(r"Opening '([^']*)'", line)
-                if match is not None:
-                    file = match.group(1)
-                    if prev_file:
-                        self.__process_file(prev_file)
-                    prev_file = file
-        for file in self.__file_list:
-            try:
-                os.unlink(file)
-            except OSError:
-                pass
-        try:
-            os.rmdir(self.__temp_dir)
-        except OSError:
-            pass
+                np_data: np.ndarray = np.frombuffer(data, FFMPEG_DATA_TYPE)
+                self.__process_chunk(np_data)
+            self.__process.wait()
 
     def stop(self) -> None:
         """Stops the thread"""
         self.__running = False
+        if self.__process is not None:
+            self.__process.terminate()
 
 def main() -> None:
     """Main function"""
@@ -176,23 +149,23 @@ def main() -> None:
                     '-m', '--model',
                     type=WhisperModel,
                     choices=[model.value for model in WhisperModel],
-                    default=WhisperModel.BASE_EN,
+                    default=DEFAULT_MODEL,
                     help=WhisperModelAnnotation.__metadata__[0]) # pylint: disable=no-member # type: ignore
     parser.add_argument(
                     '-d', '--device',
                     type=WhisperDevice,
                     choices=[device.value for device in WhisperDevice],
-                    default=WhisperDevice.CUDA,
+                    default=DEFAULT_DEVICE,
                     help=WhisperDeviceAnnotation.__metadata__[0]) # pylint: disable=no-member # type: ignore
     parser.add_argument(
                     '-l', '--chunk_length',
                     type=int,
-                    default=3,
+                    default=DEFAULT_CHUNK_LENGTH,
                     help=ChunkLengthAnnotation.__metadata__[0]) # pylint: disable=no-member # type: ignore
     parser.add_argument(
                     '-n', '--num_chunks',
                     type=int,
-                    default=2,
+                    default=DEFAULT_NUM_CHUNKS,
                     help=NumChunksAnnotation.__metadata__[0]) # pylint: disable=no-member # type: ignore
 
     args = parser.parse_args()
@@ -201,7 +174,7 @@ def main() -> None:
         stream_properties = SubtitleStreamProperties(
             device_type=args.device,
             whisper_model=args.model,
-            chunk_length=args.chunk_length,
+            chunk_duration=args.chunk_length,
             num_chunks=args.num_chunks,
             source=args.source
         )
@@ -210,8 +183,6 @@ def main() -> None:
             subtitles.start()
             subtitles.join()
         except KeyboardInterrupt:
-            pass
-        except RuntimeError:
             pass
         subtitles.stop()
         subtitles.join()
